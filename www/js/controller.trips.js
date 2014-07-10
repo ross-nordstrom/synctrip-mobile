458f925ddfd886c9fa2c22042ea592f8dafeb61a
Ross Nordstrom <nordstrom.ross@gmail.com>
2014-07-10 00:51:25 -0600

    working on connecting up Trips controller/service --- need to replace angularCollection usage with 'syncData'... research needed

--- www/js/controller.trips.js
+++ www/js/controller.trips.js
@@ -1,23 +1,23 @@
-angular.module('synctrip.controller.trips', ['simpleLoginTools'])
-.controller('TripsCtrl', function($scope) {
-  $scope.items = [
-    ['Reggae', 'Chill', 'Dubstep', 'Indie', 'Rap', 'Cowbell'],
-    ['Garifana', 'Tripstep', 'Rap', 'Hip-Hop', 'Pop', 'Top 40'],
-    ['Comedy', 'Podcasts', 'Politics', 'Sports Talks', 'Finance'],
-    ['Bob', 'John', 'Adam', 'Andy', 'Chris', 'Matthew'],
-    ['A','B','C','D','E','F','G']
-  ]
-  $scope.randomTrip = function() {
-    return $scope.items[Math.floor(Math.random()*$scope.items.length)].map(function(v) {
-      return { title: v };
-    });
-  }
-  $scope.trips = $scope.randomTrip();
+angular.module('synctrip.controller.trips', ['simpleLoginTools','synctrip.service.trips'])
+.controller('TripsCtrl', ['$scope','Trips', function($scope, Trips) {
+//  $scope.items = [
+//    ['Reggae', 'Chill', 'Dubstep', 'Indie', 'Rap', 'Cowbell'],
+//    ['Garifana', 'Tripstep', 'Rap', 'Hip-Hop', 'Pop', 'Top 40'],
+//    ['Comedy', 'Podcasts', 'Politics', 'Sports Talks', 'Finance'],
+//    ['Bob', 'John', 'Adam', 'Andy', 'Chris', 'Matthew'],
+//    ['A','B','C','D','E','F','G']
+//  ]
+//  $scope.randomTrip = function() {
+//    return $scope.items[Math.floor(Math.random()*$scope.items.length)].map(function(v) {
+//      return { title: v };
+//    });
+//  }
+  $scope.trips = Trips.collection($scope.auth.user); //$scope.randomTrip();
 
   $scope.doRefresh = function() {
     $scope.trips = $scope.randomTrip();
     $scope.$broadcast('scroll.refreshComplete');
   }
-})
+}])
 
 
