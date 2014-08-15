4f8cc79ed8908b41dd8c007b2368358ffc89ecac
Ross Nordstrom <nordstrom.ross@gmail.com>
2014-08-15 00:31:01 -0600

    edit modal allows editing of trip text

--- www/js/controller.trip.js
+++ www/js/controller.trip.js
@@ -1,7 +1,9 @@
 angular.module('synctrip.controller.trip', ['simpleLogin','synctrip.service.trips'])
-.controller('TripCtrl', ['$scope','$stateParams','Trips', 'currentUser', function($scope, $stateParams, Trips, currentUser) {
+.controller('TripCtrl', ['$scope','$stateParams','$ionicModal','Trips', 'currentUser', function($scope, $stateParams, $ionicModal, Trips, currentUser) {
   $scope.currentUser = currentUser;
   $scope.trip = Trips.find($stateParams.id);
+  $scope.showDetails = false;
+  $scope.editModal = null;
 
   $scope.doRefresh = function() {
     $scope.trip = Trips.find($scope.currentUser);
@@ -9,6 +11,20 @@ angular.module('synctrip.controller.trip', ['simpleLogin','synctrip.service.trip
       $scope.$broadcast('scroll.refreshComplete');
     });
   }
+
+  //init the modal
+  $ionicModal.fromTemplateUrl('templates/trips/edit.modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function (modal) {
+    $scope.editModal = modal;
+  });
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function () {
+    if(!!$scope.editModal) $scope.editModal.remove();
+  });
+
 }])
 
 
