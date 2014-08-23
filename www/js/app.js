e6c10cfe2846af1318c6651f4d1fa2757418eb41
Ross Nordstrom <nordstrom.ross@gmail.com>
2014-08-23 15:11:36 -0600

    google maps works

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('synctrip', ['ionic', 'firebase.utils', 'google.maps', 'synctrip.config', 'simpleLogin', 'synctrip.filters', 'synctrip.services', 'synctrip.directives', 'synctrip.controllers',
+angular.module('synctrip', ['ionic', 'firebase.utils', 'google-maps', 'synctrip.config', 'simpleLogin', 'synctrip.filters', 'synctrip.services', 'synctrip.directives', 'synctrip.controllers',
  'routeSecurity'])
 
 .run(['$ionicPlatform', 'loginService', '$rootScope', 'FBURL', function($ionicPlatform, loginService, $rootScope, FBURL) {
